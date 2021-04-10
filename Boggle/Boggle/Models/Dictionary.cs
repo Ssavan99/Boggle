40bb9247f80bf3d384f1021e7fc777b6db81bc90
rhanda2 <rhanda2@huskers.unl.edu>
2021-04-10 13:26:55 -0500

    Added the dictionary class.

---
+++ Boggle/Boggle/Models/Dictionary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Boggle.Models
+{
+    public class Dictionary
+    {
+        public List<string> DictionaryWords { set; get; }
+
+        public Dictionary CreateDictionary()
+        {
+            var logFile = File.ReadAllLines("AppData/english3.txt");
+            var logList = new List<string>();
+            foreach(var s in logFile)
+            {
+                s.Replace("\n", "");
+                logList.Add(s);
+            }
+            Dictionary dict = new Dictionary();
+            dict.DictionaryWords = logList;
+            return dict;
+        }
+        
+        public bool IsWord(string word, Dictionary dict)
+        {
+            int index = dict.DictionaryWords.BinarySearch(word);
+            if(index >= 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
