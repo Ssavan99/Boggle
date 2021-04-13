21c93e662b20e9dc9ca252a4d1e90a5ba597e93a
eyloncaplan <ecaplan2@huskers.unl.edu>
2021-04-13 16:37:15 -0500

    added unit test project

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Boggle.Models;
+
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void unitTest1()
+        {
+            //Board b = makeBoard();
+            //System.Diagnostics.Trace.WriteLine(b.getStringRepresentation());
+            Assert.IsTrue(true);
+        }
+    }
+}
