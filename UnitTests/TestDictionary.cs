57e35f0e761f654b5131facadf66e1f005693b10
rhanda2 <rhanda2@huskers.unl.edu>
2021-04-14 17:13:29 -0500

    Added WordDictionary and tests for it,

---
+++ UnitTests/TestDictionary.cs
@@ -0,0 +1,38 @@
+﻿using Boggle.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class TestDictionary
+    {
+        [TestMethod]
+        public void CreateDictTest()
+        {
+            WordDictionary dict = new WordDictionary();
+            dict.CreateDictionary();
+            int actualCount = 194433;
+            List<String> dictWords = dict.DictionaryWords;
+            int count = dictWords.Count;
+            Assert.IsTrue(actualCount.Equals(count));
+            Assert.IsTrue('a'.Equals(dictWords[0][0]));
+        }
+
+        [TestMethod]
+        public void IsWordCheckTest()
+        {
+            WordDictionary dict = new WordDictionary();
+            dict.CreateDictionary();
+            string word = "gobble";
+            Assert.IsTrue(dict.IsWord(word));
+            word = "lalalalalallal";
+            Assert.IsFalse(dict.IsWord(word));
+        }
+        
+    }
+
+}
+ 
