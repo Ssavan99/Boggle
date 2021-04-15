e59e09b7a4e790c7ec63d41cf20041720e07b885
rhanda2 <rhanda2@huskers.unl.edu>
2021-04-15 18:52:49 -0500

    Added comments

--- Boggle/Models/WordDictionary.cs
+++ Boggle/Models/WordDictionary.cs
@@ -11,7 +11,8 @@ namespace Boggle.Models
         public List<string> DictionaryWords { get; set ; }
 
         public WordDictionary()
-        {
+        {   
+            //the first path is for running tests while the second is for running the program
             //string path = Path.Combine(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, @"Boggle", @"AppData", "english3.txt");
             string path = Path.Combine(Environment.CurrentDirectory, @"AppData", "english3.txt");
             var logFile = File.ReadAllLines(path);
