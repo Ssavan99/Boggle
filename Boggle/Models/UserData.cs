6dab994eade9d6d22f486922c3df7cb8fa928da3
KH <kh@kh>
2021-04-16 16:10:52 -0500

    add UserData model

---
+++ Boggle/Models/UserData.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Boggle.Models
+{
+    public class UserData
+    {
+        int score;
+        List<string> guesses;
+
+        public UserData()
+        {
+            score = 0;
+            guesses = new List<string>();
+        }
+
+        public List<string> getGuesses()
+        {
+            return guesses;
+        }
+        public void addGuess(string guess)
+        {
+            guesses.Add(guess);
+        }
+
+        public int getScore()
+        {
+            return score;
+        }
+        public void addScore(int add)
+        {
+            score += add;
+        }
+    }
+}
